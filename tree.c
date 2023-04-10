611c18a30ad3e60c83005d048355e55b6a7fc9b6
s09890 <ary4nraj@gmail.com>
2023-04-10 19:56:29 -0400

    Final

--- tree.c
+++ tree.c
@@ -52,16 +52,54 @@ void preOrder(Tree * tr, char * filename)
   fclose (fptr);
 }
 // <<<--- UNTIL HERE
+TreeNode* newNode(int data)
+{
+  TreeNode* node = (TreeNode*) malloc(sizeof(TreeNode));
+  node->value = data;
+  node->left = node->right = NULL;
+  return (node);
+}
+
+int search(int arr[], int strt, int end, int value)
+{
+  int i;
+  for (i = strt; i <= end; i++) {
+    if (arr[i] == value)
+      break;
+  }
+  return i;
+}
+
+TreeNode* buildUtil(int in[], int post[], int inStrt, int inEnd, int* pIndex)
+{
+  if (inStrt > inEnd)
+    return NULL;
+ 
+  TreeNode* node = newNode(post[*pIndex]);
+  (*pIndex)--;
+ 
+  if (inStrt == inEnd)
+    return node;
+ 
+  int iIndex = search(in, inStrt, inEnd, node->value);
+
+  node->right = buildUtil(in, post, iIndex + 1, inEnd, pIndex);
+  node->left = buildUtil(in, post, inStrt, iIndex - 1, pIndex);
+ 
+  return node;
+}
 
 // ***
 // *** You MUST modify the follow function
 // ***
 
 #ifdef TEST_BUILDTREE
-// Consider the algorithm posted on
-// https://www.geeksforgeeks.org/construct-a-binary-tree-from-postorder-and-inorder/
-
+// Consider the algorithm I mentioned earlier
 Tree * buildTree(int * inArray, int * postArray, int size)
 {
+  int pIndex = size - 1;
+  Tree * tree = (Tree *) malloc(sizeof(Tree));
+  tree->root = buildUtil(inArray, postArray, 0, size - 1, &pIndex);
+  return tree;
 }
 #endif
